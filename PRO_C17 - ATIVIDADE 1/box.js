4d88c9a22ef1cff9cbbd89bc1fb1e82c7664e412
ProfMara <94469709+ProfMara@users.noreply.github.com>
2022-02-09 20:39:11 -0300

    Add files via upload

---
+++ PRO_C17 - ATIVIDADE 1/box.js
@@ -0,0 +1,15 @@
+class Caixa {
+    constructor() {
+        this.x = x
+        this.y = y
+        this.l = l
+        this.a = a
+    }
+
+    display() {
+        fill();
+        rect(this.x, this.y, this.l, this.a);
+    }
+
+
+}
---
+++ PRO_C17 - ATIVIDADE 1/sketch.js
@@ -0,0 +1,12 @@
+var caixa_sapato, caixa_presente, caixinha, caixona;
+
+function setup() {
+    createCanvas(400, 400);
+
+}
+
+function draw() {
+    background(53, 230, 215);
+
+
+}
